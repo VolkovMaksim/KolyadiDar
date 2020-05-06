26fe112a2e4427fde52f0ad9dcec88e15b7ab97e
Maksim Pavlov <maksan.russ@icloud.com>
2020-05-06 10:51:00 +0300

    Initial Commit

---
+++ KolyadiDar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KolyadiDar
+//
+//  Created by Maksim Pavlov on 06.05.2020.
+//  Copyright © 2020 Maksim Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
